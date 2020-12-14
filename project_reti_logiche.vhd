01c478b9865be49dbecf42a4fa1b7bef00b16a4e
giovannicaleffi <75837475+giovannicaleffi@users.noreply.github.com>
2020-12-14 16:51:07 +0100

    Update project_reti_logiche.vhd
    Versione aggiornata 14-12-2020

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -3,8 +3,8 @@
 -- Prova Finale (Progetto di Reti Logiche)
 -- Prof. William Fornaciari - Anno 2020/2021
 --
--- Filippo Caliò (Codice Persona 10628126 Matricola 907675)
--- Giovanni Caleffi (Codice Persona  Matricola )
+-- Filippo CaliÃ² (Codice Persona 10628126 Matricola 907675)
+-- Giovanni Caleffi (Codice Persona 10665233  Matricola 907455 )
 -- 
 ----------------------------------------------------------------------------------
 library IEEE;
@@ -29,25 +29,26 @@ end project_reti_logiche;
 -- BISOGNA INIZIALIZZARE A 0 TUTTI I SEGNALI
 architecture Behavioral of project_reti_logiche is
         -- segnali per l'o_address
-        signal roAddr_sel, roAddr_load : std_logic;
-        signal roAddr, mux_roAddr, o_roAddr, sum_oAddr  : std_logic_vector(15 downto 0);
+        signal roAddr_sel, roAddr_load : std_logic := '0' ;
+        signal mux_roAddr, o_roAddr, sum_oAddr  : std_logic_vector(15 downto 0) := "0000000000000000" ;
         
         -- segnali per il calcolo di righe e colonne e termine ciclo di lettura pixel
-        signal righeIn_load, righeAgg_load, righeAgg_sel, colonneIn_load, colonneAgg_sel, colonneAgg_load, o_end_righe, o_end_colonne : std_logic;
-        signal o_righeAgg, o_colonneAgg, o_righeIn, o_colonneIn : std_logic_vector(7 downto 0); --segnale in uscita dei registri
-        signal mux_righeAgg, mux_colonneAgg  : std_logic_vector (7 downto 0); --segnale in uscita dai vari multiplexer dei valori aggiornati
-        signal sub_righe, sub_colonne : std_logic_vector (7 downto 0);  --sottrattori
+        signal righeIn_load, righeAgg_load, righeAgg_sel, colonneIn_load, colonneAgg_sel, colonneAgg_load, o_end_righe, o_end_colonne : std_logic := '0';
+        signal o_righeAgg, o_colonneAgg, o_righeIn, o_colonneIn : std_logic_vector(7 downto 0) := "00000000"; --segnale in uscita dei registri
+        signal mux_righeAgg, mux_colonneAgg  : std_logic_vector (7 downto 0):= "00000000"; --segnale in uscita dai vari multiplexer dei valori aggiornati
+        signal sub_righe, sub_colonne : std_logic_vector (7 downto 0):= "00000000";  --sottrattori
 
         
         -- segnali per il calcolo del pixel massimo e minimo
-        signal pixelIn_load, pixelMin1_sel, pixelMin2_sel, pixelMin_load, o_endMin, pixelMax1_sel, pixelMax2_sel, pixelMax_load,  o_endMax   : std_logic;
-        signal o_pixelIn, o_pixelMax, o_pixelMin : std_logic_vector(7 downto 0); --segnale in uscita dei registri
-        signal mux1_pixelMax, mux1_pixelMin, mux2_pixelMin, mux2_pixelMax : STD_LOGIC_VECTOR (7 downto 0); --segnale in uscita dai mux
+        signal pixelIn_load, pixelMin1_sel, pixelMin2_sel, pixelMin_load, pixelMax1_sel, pixelMax2_sel, pixelMax_load: std_logic := '0';
+        signal o_pixelIn, o_pixelMax : std_logic_vector(7 downto 0):= "00000000"; --segnale in uscita dei registri
+        signal o_pixelMin : std_logic_vector := "11111111";
+        signal mux1_pixelMax, mux1_pixelMin, mux2_pixelMin, mux2_pixelMax : std_logic_vector (7 downto 0):= "00000000"; --segnale in uscita dai mux
 
         
         -- segnali per il delta_value
-        signal delta_value_load : std_logic;
-        signal delta_value : std_logic_vector(7 downto 0);
+        signal delta_value_load : std_logic := '0';
+        signal delta_value : std_logic_vector(7 downto 0):= "00000000"; --segnale in uscita del delta value
         
         type S is (S0, S1, S2, S3, S4, S5, S6, S7);
         signal cur_state, next_state : S;
@@ -56,19 +57,59 @@ begin
     process(i_clk, i_rst)     
         begin         
             if(i_rst = '1') then             
-            cur_state <= S0;
-            o_pixelMin <= "11111111";
-            o_pixelMax <= "00000000";
-            o_pixelIn <= "00000000";
-            delta_value <= "000000000";
-            o_righeAgg <= "000000000";
-            o_righeIn <= "000000000";
-            o_colonneAgg <= "000000000";
-            o_colonneIn <= "000000000";          
+            cur_state <= S0;       
         elsif i_clk'event and i_clk = '1' then             
             cur_state <= next_state;                   
         end if;     
     end process;
+
+    -- incremento il valore di o_addr
+    sum_oAddr <= "00000001" + o_roAddr; 
+    
+    -- creazione del mux dell' o_address
+    with roAddr_sel select
+        mux_roAddr <= "0000000000000000" when '0',
+                    sum_oAddr when '1',
+                    "XXXXXXXXXXXXXXXX" when others;
+    
+    -- processo per la gestione dell'o_address, dell'enable e dell'o_done
+    process(cur_state)
+        begin
+        if(roAddr_load = '1') then
+            o_roAddr <= mux_roAddr;
+        end  if;
+        
+        case cur_state is  
+            when S0 =>
+                roAddr_sel <= '0';
+                roAddr_load <= '1';
+            when S1 =>
+                roAddr_sel <= '1';
+                roAddr_load <= '1';
+                o_en <= '1';
+                o_we <= '0';   
+            when S2 =>
+                roAddr_sel <= '1';
+                roAddr_load <= '1';
+            when S3 =>
+                roAddr_sel <= '1';
+                roAddr_load <= '1';
+            when S4 =>
+                roAddr_sel <= '1';
+                roAddr_load <= '1';
+            when S5 =>
+                roAddr_sel <= '1';
+                roAddr_load <= '1';
+            when S6 =>
+                o_en <= '1';
+                o_we <= '1';
+                roAddr_sel <= '0';
+                roAddr_load <= '1';
+            when S7 =>
+                o_done <= '1';                     
+        end case;
+    end process; 
+    
     
      -- definizione della macchina a stati
      process(cur_state, i_start, o_end_colonne, o_end_righe)
@@ -100,7 +141,7 @@ begin
                 when S7 =>
             end case;
     end process;  
-    
+    -- multiplexer di max e min    
      with pixelMax1_sel select
         mux1_pixelMax <= o_pixelMax when '0',
                     o_pixelIn when '1',
@@ -120,18 +161,31 @@ begin
         mux2_pixelMin <= "11111111" when '0',
                     mux1_pixelMin when '1',
                     "XXXXXXXXXXXXXXXX" when others; 
+                    
+    -- uscite comparatori maggiore e minore per trovare il massimo e minimo pixel
+    pixelMax1_sel <= '1' when (o_pixelIn >= o_pixelMax) else '0';
+    
+    pixelMin1_sel <= '1' when (o_pixelIn <= o_pixelMin) else '0';                    
     
     -- processo per determinare max e min pixel e calcolare i delta_value   
     process(cur_state)
         begin
-        pixelIn_load <= '0';
-        pixelMin1_sel <= o_endMin;
-        pixelMin2_sel <= '0';
-        pixelMin_load <= '0';
-        pixelMax1_sel <= o_endMax;
-        pixelMax2_sel <= '0';
-        pixelMax_load <= '0';
-        delta_value_load <= '0';
+        if(pixelIn_load = '1') then
+            o_pixelIn <= i_data;
+        end  if;
+        
+        if(pixelMax_load = '1') then
+            o_pixelMax <= mux2_pixelMax;
+        end  if;
+        
+        if(pixelMin_load = '1') then
+            o_pixelMin <= mux2_pixelMin;
+        end  if;
+        
+        if(delta_value_load = '1') then
+            delta_value <= o_pixelMax - o_pixelMin;
+        end  if;
+        
         next_state <= cur_state;
         case cur_state is 
             when S0 =>
@@ -161,12 +215,6 @@ begin
         end case;
     end process;
     
-    -- operatore maggiore per trovare il massimo pixel
-    pixelMax1_sel <= '1' when (o_pixelIn >= o_pixelMax) else '0';
-    
-    -- operatore minore per trovare il minimo pixel
-    pixelMin1_sel <= '1' when (o_pixelIn <= o_pixelMin) else '0';
-    
     
     -- definizione dei sottrattori per decrementare #righe e #colonne
     sub_colonne <= o_colonneAgg - "00000001"; 
@@ -187,12 +235,22 @@ begin
     -- processo per la gestione di righe e colonne
     process(cur_state)
         begin
-        righeIn_load <= '0';
-        righeAgg_sel <= '0';
-        righeAgg_load <= '0';
-        colonneIn_load <= '0';
-        colonneAgg_sel <= '0';
-        colonneAgg_load <= '0';
+        if(righeIn_load = '1') then
+            o_righeIn <= i_data;
+        end  if;
+        
+        if(colonneIn_load = '1') then
+            o_colonneIn <= i_data;
+        end  if;
+        
+        if(righeAgg_load = '1') then
+            o_righeAgg <= mux_righeAgg;
+        end  if;
+        
+        if(pixelIn_load = '1') then
+            o_colonneAgg <= mux_colonneAgg;
+        end  if;
+        
         next_state <= cur_state;
         case cur_state is 
             when S0 =>
@@ -221,55 +279,6 @@ begin
                 righeAgg_load <= '1';
             when S6 =>
             when S7 =>
-        end case;
-    end process; 
-    
-    -- incremento il valore di o_addr
-    sum_oAddr <= "00000001" + o_roAddr; 
-    
-    -- creazione del mux dell' o_address
-    with roAddr_sel select
-        mux_roAddr <= "0000000000000000" when '0',
-                    sum_oAddr when '1',
-                    "XXXXXXXXXXXXXXXX" when others;
-    
-    -- processo per la gestione dell'o_address, dell'enable e dell'o_done
-    process(cur_state)
-        begin
-        o_address <= "0000000000000000";
-        roAddr_sel <= '0';
-        roAddr_load <= '0';
-        case cur_state is  
-            when S0 =>
-                roAddr_sel <= '0';
-                roAddr_load <= '1';
-            when S1 =>
-                roAddr_sel <= '1';
-                roAddr_load <= '1';
-                o_en <= '1';
-                o_we <= '0';   
-            when S2 =>
-                roAddr_sel <= '1';
-                roAddr_load <= '1';
-            when S3 =>
-                roAddr_sel <= '1';
-                roAddr_load <= '1';
-            when S4 =>
-                roAddr_sel <= '1';
-                roAddr_load <= '1';
-            when S5 =>
-                roAddr_sel <= '1';
-                roAddr_load <= '1';
-            when S6 =>
-                o_en <= '1';
-                o_we <= '1';
-                roAddr_sel <= '0';
-                roAddr_load <= '1';
-            when S7 =>
-                o_done <= '1'; 
-                
-                
-                               
         end case;
     end process;                             
 end Behavioral;
