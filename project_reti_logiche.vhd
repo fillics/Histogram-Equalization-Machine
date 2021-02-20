784947f10364c0daaf1cf2b77cd3da46ff079868
giovannicaleffi <75837475+giovannicaleffi@users.noreply.github.com>
2021-02-20 11:07:22 +0100

    Update project_reti_logiche.vhd

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -70,145 +70,81 @@ architecture Behavioral of project_reti_logiche is
         -- segnali per calcolo dell' o_data
         signal o_current_pixel_value, sub_currentPixel: std_logic_vector(7 downto 0) := "00000000";
         signal current_pixel_value_load, comparatore_sel : std_logic; 
-        signal shift_value, mux_comparatore: std_logic_vector(15 downto 0) := "0000000000000000";
+        signal shift_value: std_logic_vector(15 downto 0) := "0000000000000000";
      
 
-        type S is (S0, S1, S2, S3, S_LOOP, S5, S5bis, S6, S7, S8, S9, S10, S11, S11bis, S12, S_FINAL);
+        type S is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S_FINAL);
         signal cur_state, next_state : S;
         
         
 begin
-
     -- processo per il clock
-    process(i_clk, i_rst)     
+    process(i_clk, i_rst) 
         begin         
             if(i_rst = '1') then             
-                cur_state <= S0;       
-        elsif i_clk'event and i_clk = '1' then             
-            cur_state <= next_state;                   
-        end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin         
-            if(i_rst = '1') then             
-                o_roAddr <= "0000000000000000";    
+                cur_state <= S0;
+                o_roAddr <= "0000000000000000"; 
+                o_pixelMax <= "00000000"; 
+                o_pixelMin <= "11111111"; 
+                --o_data <= mux_comparatore(7 downto 0);  
+  
             elsif i_clk'event and i_clk = '1' then             
+                cur_state <= next_state;
                 if(roAddr_load = '1') then
-                    o_roAddr <= mux_roAddr;
+                        o_roAddr <= mux_roAddr;
                 end if;
-            end if;     
-    end process;
-    
-    process(i_clk, i_rst) -- gestione righeIn e colonneIn     
-        begin    
-            if i_clk'event and i_clk = '1' then             
+                
                 if(righeIn_load = '1') then
-                    o_righeIn <= i_data;
+                        o_righeIn <= i_data;
                 elsif (colonneIn_load = '1') then
                     o_colonneIn <= i_data;
-                end if;
-            end if;     
-    end process;
-    
-    process(i_clk, i_rst)     
-        begin    
-            if i_clk'event and i_clk = '1' then             
+                end if; 
+                
                 if(new_roAddr_load = '1') then
                     new_o_roAddr <= new_mux_roAddr;
-                end if;
-            end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin    
-            if i_clk'event and i_clk = '1' then             
+                end if; 
+                
                 if(current_pixel_value_load = '1') then
                     o_current_pixel_value <= i_data;
-                end if;
-            end if;     
-    end process;
- 
-    process(i_clk, i_rst)     
-        begin    
-            if i_clk'event and i_clk = '1' then             
+                 end if; 
+                 
                 if(new_dim_load = '1') then
                     new_dim <= o_roAddr;
                 end if;
-            end if;     
-    end process;
-    
-    process(i_clk, i_rst)     
-        begin    
-            if i_clk'event and i_clk = '1' then             
                 if(contatore_load = '1') then
                     contatore <= mux_contatore;
                 end if;
-            end if;     
-    end process;
-    
-    process(i_clk, i_rst)     
-        begin         
-            if i_clk'event and i_clk = '1' then             
+                
                 if(righeAgg_load = '1') then
                     o_righeAgg <= mux_righeAgg;
                 elsif(colonneAgg_load = '1') then
                     o_colonneAgg <= mux_colonneAgg;
-                end if;
-            end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin    
-            if i_clk'event and i_clk = '1' then             
+                end if;   
+                             
                 if(pixelIn_load = '1') then
                     o_pixelIn <= i_data;
-                end  if;
-            end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin         
-            if(i_rst = '1') then             
-                o_pixelMax <= "00000000";   
-            elsif i_clk'event and i_clk = '1' then             
+                end  if; 
+                
                 if(pixelMax_load = '1') then
                     o_pixelMax <= mux2_pixelMax;
-                end  if;
-            end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin         
-            if(i_rst = '1') then             
-                o_pixelMin <= "11111111";   
-            elsif i_clk'event and i_clk = '1' then             
+                end  if;  
+                
                 if(pixelMin_load = '1') then
                     o_pixelMin <= mux2_pixelMin;
                 end  if;
-            end if;     
-    end process;
-
-    process(i_clk, i_rst)     
-        begin         
-            if i_clk'event and i_clk = '1' then             
+                
                 if(delta_value_load = '1') then
                     delta_value <= o_pixelMax - o_pixelMin;
-                end  if;
-            end if;     
-    end process;
-    
-    process(i_clk, i_rst)     
-        begin         
-            if i_clk'event and i_clk = '1' then             
+                end  if;   
+                         
                if(shift_level_load = '1') then
                      shift_level <= "1000" -  o_floor;
-               end if;
-            end if;     
-    end process;  
-            
-    o_data <= mux_comparatore(7 downto 0);   
+               end if;                                                                      
 
+            end if;    
+    end process;
+    
+    --segnali per il cambio di inidirizzo dell'o_address durante l'assegnazione del nuovo valore del pixel
     o_address <= mux_definitivo;
     
     sub_contatore <= contatore - "0000000000000001";
@@ -241,7 +177,7 @@ begin
                     sub_contatore when '1',
                     "XXXXXXXXXXXXXXXX" when others;  
                                                 
-    -- processo per la gestione dell'o_address, dell'enable e dell'o_done
+    -- processo per la gestione dell'o_address, dell'enable e dell'o_done e del caricamento di o_data
     process(cur_state)
         begin
         roAddr_sel <= '0';
@@ -254,13 +190,14 @@ begin
         
         contatore_sel <= '0';
         contatore_load <= '0';
-        
+
         o_en <= '1';
         o_we <= '0'; 
         o_done <= '0'; 
         case cur_state is  
             when S0 =>
-                                                 
+                roAddr_sel <= '0';
+                roAddr_load <= '1';           
             when S1 =>
                 roAddr_sel <= '1';
                 roAddr_load <= '1';
@@ -272,47 +209,47 @@ begin
             when S3 =>
                 roAddr_sel <= '1';
                 roAddr_load <= '1';
-            when S_LOOP =>
+            when S4 =>
                 roAddr_sel <= '1';
                 roAddr_load <= '1';
                 
             when S5 =>
                 roAddr_sel <= '1';
-                roAddr_load <= '0'; -- possibilità di metterlo a 1
-           when S5bis =>
+                roAddr_load <= '0';
+           when S6 =>
                 roAddr_sel <= '1';
                 roAddr_load <= '1';
-            when S6 =>
+            when S7 =>
                 roAddr_sel <= '0';
                 roAddr_load <= '1';
                 new_dim_load <= '1';
-            when S7 =>
+            when S8 =>
                 roAddr_sel <= '1';
                 roAddr_load <= '1';
                 contatore_load <= '1';
-            when S8 =>
+            when S9 =>
                 roAddr_sel <= '1';
                 new_roAddr_load <= '1';
                 new_roAddr_sel <= '0';
                 contatore_sel <= '1';
-            when S9 =>
+            when S10 =>
                 mux_definitivo_sel <= '1'; 
                 contatore_sel <= '1';
                 roAddr_load <= '1';
                 roAddr_sel <= '1';
-            when S10 => 
+            when S11 => 
                 mux_definitivo_sel <= '1'; 
                 new_roAddr_sel <= '1';
                 contatore_sel <= '1';
                 roAddr_sel <= '1';
                 
-            when S11 =>
+            when S12 =>
                 new_roAddr_sel <= '1';
                 mux_definitivo_sel <= '1'; 
                 contatore_sel <= '1';
                 roAddr_sel <= '1';
                 o_we <= '1';
-            when S11bis =>
+            when S13 =>
                 new_roAddr_sel <= '1';
                 mux_definitivo_sel <= '0'; 
                 roAddr_sel <= '1';
@@ -320,12 +257,13 @@ begin
                 new_roAddr_load <= '1';
                 contatore_sel <= '1';
                 contatore_load <= '1';            
-            when S12 =>
+            when S14 =>
                 mux_definitivo_sel <= '1'; 
                 new_roAddr_sel <= '1';
                 contatore_sel <= '1';
                 roAddr_sel <= '1';
             when S_FINAL =>
+                
                 o_done <= '1';
                 o_en <= '0';                                 
         end case;
@@ -346,42 +284,43 @@ begin
                 when S2 =>
                     next_state <= S3;
                 when S3 =>
-                    next_state <= S_LOOP;
+                    next_state <= S4;
                     
-                when S_LOOP =>
+                when S4 =>
                     if o_end_colonne = '1' and o_end_righe = '1' then
-                        next_state <= S6;  
+                        next_state <= S7;  
                     elsif o_end_colonne = '1' and o_end_righe = '0' then
                         next_state <= S5;
                     else
-                        next_state <= S_LOOP;
+                        next_state <= S4;
                     end if;
                 when S5 =>
-                    next_state <= S5bis;
-                when S5bis =>
-                    next_state <= S_LOOP; 
+                    next_state <= S6;
                 when S6 =>
-                    next_state <= S7;
+                    next_state <= S4; 
                 when S7 =>
                     next_state <= S8;
                 when S8 =>
                     next_state <= S9;
                 when S9 =>
                     next_state <= S10;
-                when S10 => 
+                when S10 =>
                     next_state <= S11;
-                when S11 =>
-                    next_state <= S11bis;
-                when S11bis =>
+                when S11 => 
+                    next_state <= S12;
+                when S12 =>
+                    next_state <= S13;
+                when S13 =>
                     if(o_end_contatore = '0') then
-                        next_state <= S12;
+                        next_state <= S14;
                     else
                         next_state <= S_FINAL;
                     end if;
-                when S12 =>
-                    next_state <= S10;                    
+                when S14 =>
+                    next_state <= S11;                    
         
                 when S_FINAL =>
+                    next_state <= S0;                    
             end case;
     end process;  
     
@@ -411,7 +350,7 @@ begin
     -- processo per la gestione di righe e colonne
     process(cur_state)
         begin
-        
+                
         righeIn_load <= '0';
         righeAgg_sel <= '0';
         colonneIn_load <= '0';
@@ -432,7 +371,7 @@ begin
             when S3 =>
                 colonneAgg_load <= '1';
 
-            when S_LOOP =>
+            when S4 =>
                 colonneAgg_sel <= '1';
                 colonneAgg_load <= '1';
 
@@ -441,18 +380,17 @@ begin
                 righeAgg_load <= '1';
                 colonneAgg_load <= '1';
                 
-            when S5bis =>
+            when S6 =>
                 colonneAgg_load <= '1';
               
-            when S6 =>
             when S7 =>
             when S8 =>
             when S9 =>
-            when S10 => 
-                 
-            when S11 =>
-            when S11bis =>  
-            when S12 =>             
+            when S10 =>
+            when S11 =>              
+            when S12 =>
+            when S13 =>  
+            when S14 =>             
             when S_FINAL =>
         end case;
     end process;     
@@ -525,19 +463,16 @@ begin
         end if;
 
         case cur_state is 
-            when S0 =>
-            
+            when S0 =>           
             when S1 =>
-            
             when S2 =>
-    
             when S3 =>
                 pixelIn_load <= '1';
                 pixelMin_load <= '1';
                 
                 pixelMax2_sel <= '1';
                 pixelMax_load <= '1';
-            when S_LOOP =>
+            when S4 =>
                 pixelIn_load <= '1';
                 pixelMin_load <= '1';
                 
@@ -556,15 +491,6 @@ begin
                 pixelMax2_sel <= '1';
                 pixelMax_load <= '1';
                 
-            when S5bis =>
-                pixelIn_load <= '1';
-                pixelMin_load <= '1';
-                
-                pixelMin2_sel <= '1';
-                
-                pixelMax2_sel <= '1';
-                pixelMax_load <= '1';
-                           
             when S6 =>
                 pixelIn_load <= '1';
                 pixelMin_load <= '1';
@@ -573,21 +499,31 @@ begin
                 
                 pixelMax2_sel <= '1';
                 pixelMax_load <= '1';
-                
-                
+                           
             when S7 =>
+                pixelIn_load <= '1';
+                pixelMin_load <= '1';
+                
+                pixelMin2_sel <= '1';
+                
+                pixelMax2_sel <= '1';
+                pixelMax_load <= '1';
+                          
+            when S8 =>
                 delta_value_load <= '1'; 
                 pixelIn_load <= '1';
                 
-            when S8 =>
-                pixelIn_load <= '1';
             when S9 =>
                 pixelIn_load <= '1';
+                
+            when S10 =>
+                pixelIn_load <= '1';
                 shift_level_load <= '1';
-            when S10 =>   
-            when S11 =>
-            when S11bis =>
-            when S12 =>                                                
+                
+            when S11 =>   
+            when S12 =>
+            when S13 =>
+            when S14 =>                                                
             when S_FINAL =>
         end case;
     end process;
@@ -598,9 +534,9 @@ begin
     sub_currentPixel <= o_current_pixel_value - o_pixelMin;
     
     with comparatore_sel select
-        mux_comparatore <= "0000000011111111" when '0',
-                        shift_value when '1',
-                        "XXXXXXXXXXXXXXXX" when others; 
+        o_data <= "11111111" when '0',
+                        shift_value(7 downto 0) when '1',
+                        "XXXXXXXX" when others; 
                                  
     process(cur_state)
         begin
@@ -634,18 +570,18 @@ begin
             when S1 =>
             when S2 =>
             when S3 =>
-            when S_LOOP =>
+            when S4 =>
             when S5 =>
-            when S5bis =>
             when S6 =>
             when S7 =>
             when S8 =>
             when S9 =>
+            when S10 =>
                 current_pixel_value_load <= '1';
-            when S10 => 
-            when S11 =>                   
-            when S11bis =>
-            when S12 => 
+            when S11 => 
+            when S12 =>                   
+            when S13 =>
+            when S14 => 
                 current_pixel_value_load <= '1';
             when S_FINAL =>
             
